db090affc9d84d4fd1c066e1156dd89847e99a7d
Maksim Spirin <m.spirin@mathem.ru>
2022-05-31 15:58:49 +0300

    fix: Исправить ошибку с любым числом на кубике

--- src/utils/Event/AorB/workTemplateAOrBDice.js
+++ src/utils/Event/AorB/workTemplateAOrBDice.js
@@ -28,46 +28,47 @@ function workTemplateAOrBDice(template) {
     }
   });
 
-
-
   Object.keys(objDices).forEach((key) => {
-    if(key.includes('All')) {
-      let result = 0;
-      const newObj = {};
+    if (!isAllAll) {
+      if(key.includes('All')) {
+        let result = 0;
+        const newObj = {};
 
-      const arr = [];
-      key.split('All').forEach(((elem) => {
-        if (elem === '') arr.push('')
-        else elem.split('').forEach((item) => arr.push(item))
-      }));
+        const arr = [];
+        key.split('All').forEach(((elem) => {
+          if (elem === '') arr.push('')
+          else elem.split('').forEach((item) => arr.push(item))
+        }));
 
-      arr.forEach((elem, index) => {
-        if (elem === '') {
-          result += 1;
-          newObj[index] = false;
-        } else newObj[index] = true
-      });
-
-      const allDices = variantsDices(result);
-
-      for (let k = 0; k < Math.pow(6, result); k++) {
-        const newArr = [];
-        let j = 0;
-        Object.keys(newObj).forEach((key2,index) => {
-          if (newObj[key2]) newArr.push(String(objDices[key][index]));
-          else {
-            newArr.push(String(allDices[k][j]));
-            j += 1;
-          }
+        arr.forEach((elem, index) => {
+          if (elem === '') {
+            result += 1;
+            newObj[index] = false;
+          } else newObj[index] = true
         });
-        const newKey = newArr.join('');
-        newObjDices[newKey] = newArr;
-      }
 
-    } else {
-      newObjDices[key] = objDices[key];
+        const allDices = variantsDices(result);
+
+        for (let k = 0; k < Math.pow(6, result); k++) {
+          const newArr = [];
+          let j = 0;
+          Object.keys(newObj).forEach((key2,index) => {
+            if (newObj[key2]) newArr.push(String(objDices[key][index]));
+            else {
+              newArr.push(String(allDices[k][j]));
+              j += 1;
+            }
+          });
+          const newKey = newArr.join('');
+          newObjDices[newKey] = newArr;
+        }
+
+      } else {
+        newObjDices[key] = objDices[key];
+      }
     }
   });
+
   if (!isAllAll) {
     drawDices(dice, newObjDices);
   } else {
